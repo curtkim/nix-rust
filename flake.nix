9817431d567a9f9bf2ad1282d08b687e1df76f76
curtkim <iamteri@gmail.com>
2023-11-17 13:59:56 +0900

    format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,41 @@
 {
   description = "basic rust project";
   inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      flake-utils.url = "github:numtide/flake-utils";
-      rust-overlay.url = "github:oxalica/rust-overlay";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-      flake-utils.lib.eachDefaultSystem (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          rustVersion = pkgs.rust-bin.stable.latest.default;
-          rustPlatform = pkgs.makeRustPlatform {
-            cargo = rustVersion;
-            rustc = rustVersion;
-          };
-          myRustBuild = rustPlatform.buildRustPackage {
-            pname =
-              "nix-rust"; # make this what ever your cargo.toml package.name is
-            version = "0.1.0";
-            src = ./.; # the folder with the cargo.toml
-            cargoLock.lockFile = ./Cargo.lock;
-          };
-          dockerImage = pkgs.dockerTools.buildImage {
-            name = "nix-rust";
-            config = { Cmd = [ "${myRustBuild}/bin/nix-rust" ]; };
-          };
-        in {
-          packages = {
-            rustPackage = myRustBuild;
-            docker = dockerImage;
-          };
-          defaultPackage = dockerImage;
-          devShell = pkgs.mkShell {
-            buildInputs =
-              [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
-          };
-        });
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+        myRustBuild = rustPlatform.buildRustPackage {
+          pname =
+            "nix-rust"; # make this what ever your cargo.toml package.name is
+          version = "0.1.0";
+          src = ./.; # the folder with the cargo.toml
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "nix-rust";
+          config = { Cmd = [ "${myRustBuild}/bin/nix-rust" ]; };
+        };
+      in
+      {
+        packages = {
+          rustPackage = myRustBuild;
+          docker = dockerImage;
+        };
+        defaultPackage = dockerImage;
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
+        };
+      });
 }
