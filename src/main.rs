80c36bfdef45b1417cde16a9ec58f9afd09e6c93
curtkim <iamteri@gmail.com>
2023-11-23 11:47:29 +0900

    minor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+  let b = true;
+  let mut a = 12;
+  println!("{}", a);
+  a = 13;
+
+  println!("Hello, world!");
+  println!("{}", b);
+  println!("{}", a);
+  println!("add(1, 2) = {}", add(1,2));
+}
+
+fn add(a:u32, b:u32) ->u32{
+  return a+b;
 }
